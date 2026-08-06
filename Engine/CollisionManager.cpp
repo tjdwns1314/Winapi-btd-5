335fbea7729e949c13b0bb45f2d2b6fbbeeb8101
tjdwns1314 <tjdwns1314@gmail.com>
2026-08-06 12:26:48 +0900

    feat: CollisionManager 신설 (레이어 쌍 등록/충돌 판정)
    씬마다 충돌 판정 코드를 직접 짜지 않고, 레이어 쌍(RegisterLayer)만
    등록하면 CollisionManager가 매 프레임 해당 레이어끼리 총당 비교하며
    Actor::OnEnter를 호출해주도록 하기 위함.

---
+++ Engine/CollisionManager.cpp
@@ -0,0 +1,38 @@
+﻿// Engine/CollisionManager.cpp
+#include "pch.h"
+#include "CollisionManager.h"
+#include "Scene.h"
+#include "Actor.h"
+#include "Collider.h"
+
+void CollisionManager::RegisterLayer(RenderLayer a, RenderLayer b)
+{
+	_layerPairs.emplace_back(a, b);
+}
+
+void CollisionManager::Update(Scene& scene)
+{
+	for (const pair<RenderLayer, RenderLayer>& layers : _layerPairs)
+		checkLayer(scene, layers.first, layers.second);
+}
+
+void CollisionManager::checkLayer(Scene& scene, RenderLayer a, RenderLayer b)
+{
+	for (Actor* actorA : scene.GetActors(a))
+	{
+		if (actorA->IsPendingKill() || actorA->GetCollider() == nullptr)
+			continue;
+
+		for (Actor* actorB : scene.GetActors(b))
+		{
+			if (actorB->IsPendingKill() || actorB->GetCollider() == nullptr)
+				continue;
+
+			if (actorA->GetCollider()->CheckCollision(actorB->GetCollider()))
+			{
+				actorA->OnEnter(actorB);
+				actorB->OnEnter(actorA);
+			}
+		}
+	}
+}
