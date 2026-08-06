a5b0d8a0bcb02c0eb36c550de4d63d35acd2156a
tjdwns1314 <tjdwns1314@gmail.com>
2026-08-06 12:26:48 +0900

    feat: CollisionManager 신설 (레이어 쌍 등록/충돌 판정)
    씬마다 충돌 판정 코드를 직접 짜지 않고, 레이어 쌍(RegisterLayer)만
    등록하면 CollisionManager가 매 프레임 해당 레이어끼리 총당 비교하며
    Actor::OnEnter를 호출해주도록 하기 위함.

---
+++ Engine/CollisionManager.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Singleton.h"
+
+class Scene;
+
+class CollisionManager : public Singleton<CollisionManager>
+{
+	friend Singleton<CollisionManager>;
+
+public:
+	void RegisterLayer(RenderLayer a, RenderLayer b);
+	void Update(Scene& scene);
+private:
+	CollisionManager() = default;
+	~CollisionManager() = default;
+
+	void checkLayer(Scene& scene, RenderLayer a, RenderLayer b);
+	vector<pair<RenderLayer, RenderLayer>> _layerPairs;
+};
+
