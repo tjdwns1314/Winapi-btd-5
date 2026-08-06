fbb845822d78fda4ff0df96e8fc4f20e2d6dc7ff
tjdwns1314 <tjdwns1314@gmail.com>
2026-08-06 19:30:26 +0900

    feat: 풍선 터짐 처리를 위한 BloonPopResolver 골격 추가
    1. 풍선 색상별 레이어 HP를 반환하는 GetLayerHp 정의
    2. 터진 풍선 색상과 남은 데미지로 하위 풍선 생성 여부를 계산하는 Resolve 골격 추가(구현 미완성)
    3. BloonPopResolver.cpp/.h를 TowerDefense.vcxproj에 등록

---
+++ BloonPopResolver.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "BloonType.h"
+class BloonPopResolver
+{
+public:
+	struct Result
+	{
+		bool hasNext = false;
+		BloonColor color = BloonColor::Red;
+		int32 hp = 0;
+	};
+
+	static int32 GetLayerHp(BloonColor color);
+	static Result Resolve(BloonColor popedColor, int32 leftoverDamage);
+};
+
