40558fcf236e9330e8f60bcbc7a91742055360fa
tjdwns1314 <tjdwns1314@gmail.com>
2026-08-07 16:58:57 +0900

    refactor: Scene이 CollisionManager를 직접 소유하고 UIManager 연동 훅 추가
    1. CollisionManager를 싱글톤에서 제거하고 Scene이 멤버로 소유하도록 변경
    2. Scene::Init/Cleanup/Update/Render에 UIManager Clear/CreateUI/Update/Render 연동
    3. 파생 씬이 오버라이드할 CreateUI() 가상 함수 추가

--- Engine/Collision/CollisionManager.h
+++ Engine/Collision/CollisionManager.h
@@ -1,24 +1,23 @@
 ﻿#pragma once
-#include "Singleton.h"
+
 
 class Scene;
 class Actor;
 
-class CollisionManager : public Singleton<CollisionManager>
+class CollisionManager 
 {
-	friend Singleton<CollisionManager>;
 
 public:
 	void RegisterLayer(RenderLayer a, RenderLayer b);
 	void Update(Scene& scene);
-
+	CollisionManager() = default;
+	~CollisionManager() = default;
 	// Scene::PostUpdate가 액터를 delete/Pool 반환하기 직전에 호출한다.
 	// 다음 프레임까지 기다려 Exit를 판정하면 댕글링/풀 재사용 문제가 생기므로
 	// 파괴 직전 이 시점에 즉시 OnExit를 통지하고 추적 목록에서 제거한다.
 	void RemoveActor(Actor* actor);
 private:
-	CollisionManager() = default;
-	~CollisionManager() = default;
+
 
 	void checkLayer(Scene& scene, RenderLayer a, RenderLayer b);
 
