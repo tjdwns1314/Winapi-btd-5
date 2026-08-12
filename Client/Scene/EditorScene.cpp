8a8292b9949b7f7e7d25145c9f3294cf6e97a3ce
tjdwns1314 <tjdwns1314@gmail.com>
2026-08-12 11:17:52 +0900

    chore: 에디터 씬에 다트 몽키/스나이퍼 몽키 베이크 결과 확인용 드로우 추가
    1. EditorScene::Render에 dart_monkey_baked, sniper_monkey_baked를 직접 그려서 베이킹 결과를 눈으로 확인하는 코드 추가
    2. 개별 스프라이트 조각(sniper_arm, dart_monkey_body 등)을 하나씩 그려보며 오프셋을 맞추던 이전 테스트 코드는 주석 처리로 남겨둠 - 정리 필요, 미검증 상태로 방치 중

--- Client/Scene/EditorScene.cpp
+++ Client/Scene/EditorScene.cpp
@@ -25,10 +25,39 @@ void EditorScene::Render(Graphic& graphic)
 
 	ResourceManager::GetInstance().GetImage(L"tack_shooter_baked").Draw(graphic, 400.0f, 200.0f, 1.0f, 0.0f);
 	ResourceManager::GetInstance().GetImage(L"bomb_tower_baked").Draw(graphic, 400.0f, 400.0f, 1.0f, 0.0f);
+	ResourceManager::GetInstance().GetImage(L"dart_monkey_baked").Draw(graphic, 400.0f, 600.0f, 1.0f, 0.0f);
+	ResourceManager::GetInstance().GetImage(L"sniper_monkey_baked").Draw(graphic, 600.0f, 600.0f, 1.0f, 0.0f);
 
-	if (const CellInfo* cell = _sprite->GetCell("bomb_tower_wheel"))
-		 {
+
+
+	/*if (const CellInfo* cell = _sprite->GetCell("sniper_arm"))
+	{
+		_inGameBg->DrawSprite(graphic, 695.0f, 640.0f, *cell, 1.0f, 0.0f);
+	}
+	if (const CellInfo* cell = _sprite->GetCell("dart_monkey_body"))
+	{
 		_inGameBg->DrawSprite(graphic, 700.0f, 700.0f, *cell, 1.0f, 0.0f);
+	}
+
+	if (const CellInfo* cell = _sprite->GetCell("sniper_green_hat"))
+		 {
+		_inGameBg->DrawSprite(graphic, 695.0f, 695.0f, *cell, 1.0f, 0.0f);
 		}
+	if (const CellInfo* cell = _sprite->GetCell("sniper_basic_rifle"))
+	{
+		_inGameBg->DrawSprite(graphic, 675.0f, 630.0f, *cell, 1.0f, 0.0f);
+	}
+
+
+
+	if (const CellInfo* cell = _sprite->GetCell("dart_monkey_arm_01"))
+	{
+		_inGameBg->DrawSprite(graphic, 500.0f, 500.0f, *cell, 1.0f, 0.0f);
+	}
+	if (const CellInfo* cell = _sprite->GetCell("dart_monkey_body"))
+	{
+		_inGameBg->DrawSprite(graphic, 500.0f, 500.0f, *cell, 1.0f, 0.0f);
+	}*/
+
 }
 
