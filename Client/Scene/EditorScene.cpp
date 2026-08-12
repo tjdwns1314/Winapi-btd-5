4629cc563292fdedc9504d4d7a2138e6bf224b12
tjdwns1314 <tjdwns1314@gmail.com>
2026-08-12 11:59:32 +0900

    chore: 에디터 씬 베이크 결과 확인용 드로우를 새 스프라이트 키로 갱신
    1. 이전 커밋에서 추가했던 다트 몽키/스나이퍼 몽키 파츠 확인용 주석 처리 코드를 정리하고, 압정슈터 압정("tack_shooter_tack")과 폭탄타워 폭탄("bomb_tower_bomb_01") 스프라이트를 눈으로 확인할 수 있도록 드로우 코드로 교체함

--- Client/Scene/EditorScene.cpp
+++ Client/Scene/EditorScene.cpp
@@ -30,34 +30,19 @@ void EditorScene::Render(Graphic& graphic)
 
 
 
-	/*if (const CellInfo* cell = _sprite->GetCell("sniper_arm"))
+	if (const CellInfo* cell = _sprite->GetCell("tack_shooter_tack"))
 	{
 		_inGameBg->DrawSprite(graphic, 695.0f, 640.0f, *cell, 1.0f, 0.0f);
 	}
-	if (const CellInfo* cell = _sprite->GetCell("dart_monkey_body"))
-	{
-		_inGameBg->DrawSprite(graphic, 700.0f, 700.0f, *cell, 1.0f, 0.0f);
-	}
-
-	if (const CellInfo* cell = _sprite->GetCell("sniper_green_hat"))
-		 {
-		_inGameBg->DrawSprite(graphic, 695.0f, 695.0f, *cell, 1.0f, 0.0f);
-		}
-	if (const CellInfo* cell = _sprite->GetCell("sniper_basic_rifle"))
-	{
-		_inGameBg->DrawSprite(graphic, 675.0f, 630.0f, *cell, 1.0f, 0.0f);
-	}
 
 
 
-	if (const CellInfo* cell = _sprite->GetCell("dart_monkey_arm_01"))
+
+	if (const CellInfo* cell = _sprite->GetCell("bomb_tower_bomb_01"))
 	{
 		_inGameBg->DrawSprite(graphic, 500.0f, 500.0f, *cell, 1.0f, 0.0f);
 	}
-	if (const CellInfo* cell = _sprite->GetCell("dart_monkey_body"))
-	{
-		_inGameBg->DrawSprite(graphic, 500.0f, 500.0f, *cell, 1.0f, 0.0f);
-	}*/
+
 
 }
 
