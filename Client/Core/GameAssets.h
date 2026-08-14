a7fa41bcfecb8abc910d75f178328ef5f06385d3
tjdwns1314 <tjdwns1314@gmail.com>
2026-08-14 12:32:03 +0900

    refactor: ResourceManager의 리소스 목록/베이킹 지식을 GameAssets로 분리
    1. ResourceManager(Engine 계층)가 갖고 있던 "어떤 리소스를 로드하고 어떻게 구울지"라는 게임 고유 지식(Preload/Init)을 Client/Core/GameAssets::Load로 옮기고, ResourceManager는 LoadImage/LoadAtlas/GetImage/GetAtlas/Cleanup/BakeImage라는 범용 로드·조회·굽기 도구만 남기도록 축소
    2. Game::Init()의 ResourceManager::Init(_graphic) 호출을 GameAssets::Load(_graphic)로 교체
    3. Game::Cleanup()이 지금까지 ResourceManager::Cleanup()을 한 번도 호출하지 않고 있던 것을 발견해서, _graphic.Cleanup()보다 먼저 호출하도록 추가(COM 리소스 해제 순서상 그래픽 팩토리/렌더타깃을 죽이기 전에 이미지부터 정리)
    4. TowerDefense.vcxproj/.filters에 신규 파일 등록

---
+++ Client/Core/GameAssets.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+class Graphic;
+
+class GameAssets
+{
+public:
+	static void Load(Graphic& graphic);
+};
--- Engine/Manager/ResourceManager.h
+++ Engine/Manager/ResourceManager.h
@@ -8,14 +8,9 @@ class ResourceManager : public Singleton<ResourceManager>
 	friend class Singleton<ResourceManager>;
 
 public :
-	// Preload로 원본 리소스를 불러온 뒤, 필요한 스프라이트들을 합성해 텍스처로 구워둔다.
-	// Game::Init에서 이 함수 하나만 호출하면 Preload + 굽기가 한 번에 끝난다.
-	void Init(Graphic& graphic);
+	void LoadImage(Graphic& graphic, const wchar_t* fileName);
+	void LoadAtlas(const wchar_t* xmlPath);
 
-	// 게임 시작 시 필요한 리소스를 한 번에 전부 로드한다.
-	void Preload(Graphic& graphic); //메인씬 나올때 로딩씬쪽에다가 Preload로 리소스를 다 가져오자
-
-	// 이미 로드된 리소스를 이름으로 조회만 한다 (로딩은 Preload에서만 일어난다).
 	Image& GetImage(const wchar_t* fileName);
 	SpriteAtlas& GetAtlas(const wchar_t* xmlPath);
 
@@ -25,9 +20,6 @@ public :
 
 private :
 	ResourceManager() = default;
-
-	void loadImage(Graphic& graphic, const wchar_t* fileName);
-	void loadAtlas(const wchar_t* xmlPath);
 
 	unordered_map<wstring, unique_ptr<Image>> _images;
 	unordered_map<wstring, unique_ptr<SpriteAtlas>> _atlases;
