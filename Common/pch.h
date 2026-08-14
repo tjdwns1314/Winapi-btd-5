3282abc03ad193df5ef5aa3e390f0380f8a31d39
tjdwns1314 <tjdwns1314@gmail.com>
2026-08-14 12:27:13 +0900

    refactor: Common/pch.h를 VectorMath.h/GameConfig.h로 분리
    1. pch.h에 있던 정수 타입 별칭과 Vector/Cell 구조체를 Common/VectorMath.h로, 게임 상수(GWinSizeX, BLOCK_SIZE 등)를 Common/GameConfig.h로 분리해서, pch.h는 STL include와 using 선언만 남기고 가벼워지도록 함
    2. using namespace std;를 제거하고, 실제로 코드베이스 전체에서 std:: 접두어 없이 쓰이는 이름이 있는지 grep으로 전수 확인한 뒤 그 목록만 개별 using 선언으로 남겨서 전역 네임스페이스 오염 위험을 줄임
    3. Cell::ConvertToCEll 오타를 ConvertToCell로 수정하고, 이 함수를 호출하던 Grid::UpdateGrid의 두 호출부도 이름을 맞춰 수정

--- Common/pch.h
+++ Common/pch.h
@@ -15,153 +15,18 @@
 #include <limits>
 
 #include "Util.h"
-using namespace std;
+#include "VectorMath.h"
+#include "GameConfig.h"
 
-using int8 = char;		// 1byte 정수형
-using int16 = short;	// 2byte 정수형
-using int32 = int;		// 4byte 정수형
-using int64 = long long;// 8byte 정수형
-
-using uint8 = unsigned char;		// 1byte 정수형
-using uint16 = unsigned short;	// 2byte 정수형
-using uint32 = unsigned int;		// 4byte 정수형
-using uint64 = unsigned long long;// 8byte 정수형
-
-constexpr int32 GWinSizeX = 1760;
-constexpr int32 GWinSizeY = 1040;
-constexpr int32 GameAreaWidth = 1440;
-constexpr int32 GameAreaHeight = 1040;
-constexpr int32 BLOCK_SIZE = 80;
-constexpr int32 GRID_COUNT_X = GameAreaWidth / BLOCK_SIZE;
-constexpr int32 GRID_COUNT_Y = GameAreaHeight / BLOCK_SIZE;
-static_assert(GRID_COUNT_X* BLOCK_SIZE == GameAreaWidth, "BLOCK_SIZE가 GameAreaWidth를 나누어떨어뜨리지 않습니다");
-static_assert(GRID_COUNT_Y* BLOCK_SIZE == GameAreaHeight, "BLOCK_SIZE가 GameAreaHeight를 나누어떨어뜨리지 않습니다");
-constexpr int32 GameAreaCenterX = GameAreaWidth / 2;
-constexpr int32 GameAreaCenterY = GameAreaHeight / 2;
-
-
-
-#define SMALL_NUMBER			(1.e-8f)
-
-struct Vector
-{
-	float x = 0;
-	float y = 0;
-
-	Vector() {}
-	Vector(float x, float y) : x(x), y(y) {}
-	Vector(POINT pt) : x((float)pt.x), y((float)pt.y) {}
-
-	Vector operator+(const Vector& other)
-	{
-		Vector ret;
-		ret.x = x + other.x;
-		ret.y = y + other.y;
-		return ret;
-	}
-
-	Vector operator-(const Vector& other)
-	{
-		Vector ret;
-		ret.x = x - other.x;
-		ret.y = y - other.y;
-		return ret;
-	}
-
-	Vector operator*(float value)
-	{
-		Vector ret;
-		ret.x = x * value;
-		ret.y = y * value;
-		return ret;
-	}
-	void operator+=(const Vector& other)
-	{
-		x += other.x;
-		y += other.y;
-	}
-
-	void operator-=(const Vector& other)
-	{
-		x -= other.x;
-		y -= other.y;
-	}
-
-	void operator*=(float ratio)
-	{
-		x *= ratio;
-		y *= ratio;
-	}
-
-	bool operator==(const Vector& other) const
-	{
-		return (x == other.x && y == other.y);
-	}
-
-	bool operator==(Vector& other)
-	{
-		return (x == other.x && y == other.y);
-	}
-
-
-	// 내적
-	// 외적
-	// 정규화
-	// 크기
-	float LengthSquared()
-	{
-		return x * x + y * y;
-	}
-	float Length()
-	{
-		return ::sqrt(LengthSquared());
-	}
-
-	void Normalize()
-	{
-		float length = Length();
-		if (length < SMALL_NUMBER)
-			return;
-
-		x /= length;
-		y /= length;
-	}
-
-	// Dot 내적
-	// float = cos(A)
-	float Dot(Vector other)
-	{
-		return x * other.x + y * other.y;
-	}
-
-	// Cross 외적
-	// 결과가 3D 벡터여야하는데, 2D 좌표계라서 Z=0, float 하나를 리턴시킨다. (0,0,x)
-	float Cross(Vector other)
-	{
-		return x * other.y - y * other.x;
-	}
-
-	Vector Rotate(float radian)
-	{
-		float cosA = cosf(radian);
-		float sinA = sinf(radian);
-
-		return Vector(x * cosA - y * sinA, x * sinA + y * cosA);
-	}
-};
-
-
-
-struct Cell
-{
-	int32 iX = 0;
-	int32 iY = 0;
-
-	static Cell ConvertToCEll(Vector pos, int32 size)
-	{
-		if (pos.x < 0 || pos.y < 0)
-			return Cell{ -1,-1 };
-
-		return Cell{ (int32)pos.x / size,(int32)pos.y / size };
-	}
-};
+using std::string;
+using std::wstring;
+using std::vector;
+using std::set;
+using std::unordered_map;
+using std::queue;
+using std::priority_queue;
+using std::pair;
+using std::function;
+using std::unique_ptr;
+using std::make_unique;
+using std::move;
